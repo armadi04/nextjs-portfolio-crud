dcead6f8546c4fa22df49890f2b5d9e9e48cbfa7
Armadi Nurhidayat <armadinurhidayat04@gmail.com>
2026-02-05 18:24:50 +0700

    add

--- scripts/check-bio.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {
-  const profile = await prisma.profile.findFirst();
-  console.log("Raw Bio:", JSON.stringify(profile?.bio));
-}
-main();
